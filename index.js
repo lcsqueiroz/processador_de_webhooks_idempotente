4240247d810512120a35b914fd93d2962fbb36f7
Lucas Queiroz <lqvieira7@gmail.com>
2026-07-13 19:45:05 -0300

    feat: rotas e testes indexados ao index.js

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import router from './routes/payment.js';
+import evaluationRouter from './routes/evaluation.js';
+import payload from './script/payload.js';
 
 const app = express();
 const port = 3000;
@@ -7,6 +9,10 @@ const port = 3000;
 app.use(express.json());
 
 app.use(router);
+app.use(evaluationRouter);
+
+// remova o payload() se quiser fazer testes unitários
+payload();
 
 app.listen(port, () => {
   console.log(`Servidor rodando na porta ${port}`);
