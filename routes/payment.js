2b14568ee541c2de6c5c4bc4125d72a2c661327d
Lucas Queiroz <lqvieira7@gmail.com>
2026-07-13 19:25:29 -0300

    feat: utilização da função para verificar key

--- routes/payment.js
+++ routes/payment.js
@@ -1,9 +1,20 @@
 import { Router } from 'express';
+import validateWebhook from '../middleware/validateWebhook.js';
+import { processarPagamento } from '../service/paymentService.js';
 
 const router = Router();
 
-router.post('/webhooks/payment', (req, res) => {
-  return res.status(200).json({ message: 'Tudo certo' });
+router.post('/webhooks/payment', validateWebhook, (req, res) => {
+  const idempotencyKey = req.get('Idempotency-Key');
+
+  const { isReplay, resultado } = processarPagamento(idempotencyKey, req.body);
+
+  if (isReplay) {
+    res.set('Idempotency-Replay', 'true');
+    return res.status(200).json(resultado);
+  }
+
+  return res.status(201).json(resultado);
 });
 
 export default router;
